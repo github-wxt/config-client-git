feb8ae06d5da700ceeabd012ffce5851c6818ce8
github-wxt <675632387@qq.com>
2021-06-14 21:07:27 +0800

    first commit

---
+++ src/main/java/com/wxt/configclientgit/ConfigClientGitApplication.java
@@ -0,0 +1,19 @@
+package com.wxt.configclientgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈类描述〉
+ * 〈〉
+ *
+ * @author 67563
+ * @create 2021/6/14
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ConfigClientGitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientGitApplication.class,args);
+    }
+}
